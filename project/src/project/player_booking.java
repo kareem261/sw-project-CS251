fea1fd18e196f38b8c741c85a895f5c48777abe5
kareem mostafa <kareemabdo9864@gmail.com>
2020-05-20 12:48:12 -0700

    third version class player_bookings

---
+++ project/src/project/player_booking.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import java.util.Vector;
+
+/**
+ *class that has only to attributes playground_name and hours
+ * "hours is a vector that holds all bookings hours for reach playground"
+ * @author EGYPT_LAPTOP
+ */
+public class player_booking {
+  
+    String playground_name;
+    Vector<Integer>hours= new Vector <Integer> (6);
+
+
+}
